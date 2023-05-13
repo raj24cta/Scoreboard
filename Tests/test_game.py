13355d9197b8e8811fef666082edcd4b76a0a323
raj24cta <83284888+raj24cta@users.noreply.github.com>
2023-05-13 11:58:04 +0200

    Update test_game.py

--- Tests/test_game.py
+++ Tests/test_game.py
@@ -1,23 +1,49 @@
 import unittest
-from football_scoreboard.game import Game
+from scoreboard.scoreboard import Scoreboard
 
 
 class TestGame(unittest.TestCase):
-    def test_game_init(self):
-        game = Game("Spain", "Brazil")
-        self.assertEqual(game.home_team, "Spain")
-        self.assertEqual(game.away_team, "Brazil")
-        self.assertEqual(game.home_score, 0)
-        self.assertEqual(game.away_score, 0)
+    def setUp(self):
+        self.scoreboard = Scoreboard()
 
-    def test_game_update_score(self):
-        game = Game("Spain", "Brazil")
-        game.update_score(2, 1)
-        self.assertEqual(game.home_score, 2)
-        self.assertEqual(game.away_score, 1)
+    def test_start_match(self):
+        self.scoreboard.start_match("Mexico", "Canada")
+        matches = self.scoreboard.get_summary()
+        self.assertEqual(len(matches), 1)
+        match = matches[0]
+        self.assertEqual(match.home_team, "Mexico")
+        self.assertEqual(match.away_team, "Canada")
+        self.assertEqual(match.home_score, 0)
+        self.assertEqual(match.away_score, 0)
 
-    def test_game_get_total_score(self):
-        game = Game("Spain", "Brazil")
-        game.update_score(2, 1)
-        self.assertEqual(game.get_total_score(), 3)
+    def test_update_score(self):
+        self.scoreboard.start_match("Mexico", "Canada")
+        self.scoreboard.update_score("Mexico", "Canada", 2, 1)
+        matches = self.scoreboard.get_summary()
+        self.assertEqual(len(matches), 1)
+        match = matches[0]
+        self.assertEqual(match.home_score, 2)
+        self.assertEqual(match.away_score, 1)
 
+    def test_finish_match(self):
+        self.scoreboard.start_match("Mexico", "Canada")
+        self.scoreboard.finish_match("Mexico", "Canada")
+        matches = self.scoreboard.get_summary()
+        self.assertEqual(len(matches), 0)
+
+    def test_get_summary(self):
+        self.scoreboard.start_match("Mexico", "Canada")
+        self.scoreboard.start_match("Spain", "Brazil")
+        self.scoreboard.update_score("Mexico", "Canada", 3, 1)
+        self.scoreboard.update_score("Spain", "Brazil", 2, 2)
+        matches = self.scoreboard.get_summary()
+        self.assertEqual(len(matches), 2)
+        self.assertEqual(matches[0].home_team, "Mexico")
+        self.assertEqual(matches[0].away_team, "Canada")
+        self.assertEqual(matches[1].home_team, "Spain")
+        self.assertEqual(matches[1].away_team, "Brazil")
+        self.assertGreater(matches[0].home_score, matches[1].home_score)
+
+
+if __name__ == '__main__':
+    unittest.main()
