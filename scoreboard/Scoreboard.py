1502a6268a392e748023c2a7af77f9526b6c703d
raj24cta <83284888+raj24cta@users.noreply.github.com>
2023-05-13 11:32:24 +0200

    Update Scoreboard.py

--- scoreboard/Scoreboard.py
+++ scoreboard/Scoreboard.py
@@ -1,43 +1,18 @@
-from collections import defaultdict
-
-
 class Scoreboard:
     def __init__(self):
-        self.matches = []
+        self.games = []
 
     def start_game(self, home_team, away_team):
         game = Game(home_team, away_team)
-        self.matches.append(game)
+        self.games.append(game)
 
-    def update_score(self, home_score, away_score):
-        if len(self.matches) == 0:
-            raise ValueError("No matches in progress")
+    def update_score(self, game_index, home_score, away_score):
+        game = self.games[game_index]
+        game.update_score(home_score, away_score)
 
-        current_game = self.matches[-1]
-        current_game.update_score(home_score, away_score)
-
-    def finish_game(self):
-        if len(self.matches) == 0:
-            raise ValueError("No matches in progress")
-
-        self.matches.pop()
+    def finish_game(self, game_index):
+        self.games.pop(game_index)
 
     def get_summary(self):
-        summary = sorted(self.matches, key=lambda x: x.total_score(), reverse=True)
-        return summary
-
-
-class Game:
-    def __init__(self, home_team, away_team):
-        self.home_team = home_team
-        self.away_team = away_team
-        self.home_score = 0
-        self.away_score = 0
-
-    def update_score(self, home_score, away_score):
-        self.home_score = home_score
-        self.away_score = away_score
-
-    def total_score(self):
-        return self.home_score + self.away_score
-
+        sorted_games = sorted(self.games, key=lambda g: (-g.get_total_score(), -self.games.index(g)))
+        return "\n".join([f"{i+1}. {game}" for i, game in enumerate(sorted_games)])
