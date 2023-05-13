46b4d145d5b3a9224ccc5bc021edd7b612945a2c
raj24cta <83284888+raj24cta@users.noreply.github.com>
2023-05-13 11:31:38 +0200

    Update Game.py

--- scoreboard/Game.py
+++ scoreboard/Game.py
@@ -1,1 +1,17 @@
+class Game:
+    def __init__(self, home_team, away_team):
+        self.home_team = home_team
+        self.away_team = away_team
+        self.home_score = 0
+        self.away_score = 0
+
+    def update_score(self, home_score, away_score):
+        self.home_score = home_score
+        self.away_score = away_score
+
+    def get_total_score(self):
+        return self.home_score + self.away_score
+
+    def __str__(self):
+        return f"{self.home_team} {self.home_score} - {self.away_team} {self.away_score}"
 
